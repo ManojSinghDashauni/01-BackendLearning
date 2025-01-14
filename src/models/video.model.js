fbb778b5074ad33d04bbcd86255deeefbed80c13
manoj singh dashauni <dashauni007@gmail.com>
2025-01-15 00:10:30 +0530

    work on schema and jwt, bcrypt and mongoose ismodified and custom method

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; //it use for aaggregation mongodb it also install 
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String, 
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate) //when can write aggregation query
+
+export const Video = mongoose.model("Video", videoSchema)
