7ee58760717f823e856422ae44b322d301732282
manoj singh dashauni <dashauni007@gmail.com>
2025-01-15 14:41:12 +0530

    add register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {registerUser} from"../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 //https://localhots:8000/apo/v1/users/register
-router.route("/register").post(registerUser)
+//add middleware upload
+router.route("/register").post(
+    upload.fields([
+        {name:"avatar",maxCount:1},
+        {name:"coverImage",maxCount:1}
+    ]),
+    registerUser)
 
 export default router
