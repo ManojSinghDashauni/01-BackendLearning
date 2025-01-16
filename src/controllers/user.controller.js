bb58939a75bb2abebd2d6d1af2e640ae32c75887
manoj singh dashauni <dashauni007@gmail.com>
2025-01-16 15:28:26 +0530

    create getuser update account details , avatar and cover image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 
@@ -235,4 +235,143 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword, confiremedPassword } = req.body;
+
+  if (!(newPassword === confiremedPassword)) {
+    throw new ApiError(400, "password is not match");
+  }
+
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid old password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,req.user,"current user fetched succesfully")
+  )
+})
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName,email} = req.body;
+  const user = User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set : {
+        fullName,
+        email
+      }
+    },
+    {new:true}
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,user,"account detail updated succesfully")
+  )
+})
+
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+  const avatarLocalPath = req.file?.path
+
+  if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar file is missing")
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+  if (!avatar.url) {
+      throw new ApiError(400, "Error while uploading on avatar")
+  }
+
+ 
+
+
+  const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+          $set:{
+              avatar: avatar.url
+          }
+      },
+      {new: true}
+  ).select("-password")
+
+  //  //TODO: delete old image - assignment
+
+  //  const oldAvatarLocalPath = await user?.avatar?.url
+ 
+  //  const result = await cloudinaryDeleteOldFile(oldAvatarLocalPath);
+   
+  //  if(!result){
+  //    throw new ApiError(400, "Error delelte uploading on avatar")
+  //  }
+ 
+  //  //end assignment
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, user, "Avatar image updated successfully")
+  )
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+  const coverImageLocalPath = req.file?.path
+
+  if (!coverImageLocalPath) {
+      throw new ApiError(400, "Cover image file is missing")
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if (!coverImage.url) {
+      throw new ApiError(400, "Error while uploading on avatar")
+      
+  }
+
+  const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+          $set:{
+              coverImage: coverImage.url
+          }
+      },
+      {new: true}
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, user, "Cover image updated successfully")
+  )
+})
+
+
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+};
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,32 +1,45 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 //fibonaccifire cloudinary
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        // console.log("file is uploaded on cloudinary ", response.url);
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    // console.log("file is uploaded on cloudinary ", response.url);
 
-        fs.unlinkSync(localFilePath)
-        return response;
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove file locally saved
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove file locally saved 
-        return null;
-    }
-}
+// const cloudinaryDeleteOldFile = async (FilePath) => {
+//   try {
+//     // Delete the old file (if a public_id is provided)
+//     if (FilePath) {
+//       const deleteResponse = await cloudinary.uploader.destroy(FilePath);
+//       console.log("Old file deleted:", deleteResponse);
+//     }
 
+//     return true
 
+//   } catch (error) {
+//     console.error("Error during upload or delete:", error);
+//     throw error;
+//   }
+// };
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
