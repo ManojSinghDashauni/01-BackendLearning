2d43c414a30dec605339ba29844de33b1f0d1ee8
manoj singh dashauni <dashauni007@gmail.com>
2025-01-16 21:41:30 +0530

    nested aggregation

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,45 @@
 import { Router } from "express";
-import {loginUser, logoutUser, refreshAccessToken, registerUser, updateUserAvatar, updateUserCoverImage} from"../controllers/user.controller.js"
+import {
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 //https://localhots:8000/apo/v1/users/register
 //add middleware upload
 router.route("/register").post(
-    upload.fields([
-        {name:"avatar",maxCount:1},
-        {name:"coverImage",maxCount:1}
-    ]),
-    registerUser)
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 router.route("/login").post(loginUser)
-
-router.route("/logout").post(verifyJWT,logoutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
-export default router
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+export default router;
